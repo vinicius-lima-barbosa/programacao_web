b57dbfdb053affae858998a6c0e157621597284e
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-09-16 22:11:35 -0300

    feat: updated scripts

--- atividade03/scripts.js
+++ atividade03/scripts.js
@@ -1,26 +1,44 @@
-const button = document.querySelector('.button');
-const result = document.querySelector('.resultados');
+const button = document.querySelector(".button");
+const result = document.querySelector(".resultados");
+const input = document.querySelector(".input-area");
 
-button.addEventListener('click', async () => {
-    const cep = document.querySelector('.input-area').value;
-    if (cep.length !== 8 || isNaN(cep)) { alert('Requisiçao invalida'); return; }
+const searchCEP = async (cep) => {
+  if (cep.length !== 8 || isNaN(cep)) {
+    alert("Requisiçao invalida");
+    return;
+  }
 
-    try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        if (!response.ok) throw new Error('erro no fetch');
-        const data = await response.json();
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    if (!response.ok) throw new Error("erro no fetch");
+    const data = await response.json();
 
-        if (data.erro) { alert('CEP nao encontrado'); return; }
-
-        result.innerHTML = `
-            <h3>Resultados</h3>
-            <p><b>Rua</b>: <span class="content">${data.logradouro}</span></p>
-            <p><b>Bairro</b>: <span class="content">${data.bairro}</span></p>
-            <p><b>Cidade</b>: <span class="content">${data.localidade}</span></p>
-            <p><b>Estado</b>: <span class="content">${data.estado}</span></p>
-            <p><b>UF</b>: <span class="content">${data.uf}</span></p>
-        `;
-    } catch (e) {
-        alert(`Erro: ${e}`);
+    if (data.erro) {
+      alert("CEP nao encontrado");
+      return;
     }
+
+    result.innerHTML = `
+                <h3>Resultados</h3>
+                <p><b>Rua</b>: <span class="content">${data.logradouro}</span></p>
+                <p><b>Bairro</b>: <span class="content">${data.bairro}</span></p>
+                <p><b>Cidade</b>: <span class="content">${data.localidade}</span></p>
+                <p><b>Estado</b>: <span class="content">${data.estado}</span></p>
+                <p><b>UF</b>: <span class="content">${data.uf}</span></p>
+            `;
+  } catch (e) {
+    alert(`Erro: ${e}`);
+  }
+};
+
+button.addEventListener("click", () => {
+  const cep = input.value;
+  searchCEP(cep);
+});
+
+input.addEventListener("keypress", (e) => {
+  if (e.code === "Enter") {
+    const cep = input.value;
+    searchCEP(cep);
+  }
 });
